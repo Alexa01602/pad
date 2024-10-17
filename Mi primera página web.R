ca8131fa47b4632a387c86d28d66bc996f71a0c1
unknown <alezajiyo02@gmail.com>
2024-10-16 21:02:38 -0500

    mi primera página web

---
+++ Mi primera página web.R
@@ -0,0 +1,11 @@
+
+# Importar datos en csv ---------------------------------------------------
+
+datos <- read.csv("Datos/LA MOLINA 2014 POTATO WUE (FB) - fb.csv"
+                  , header = TRUE, sep = ",")
+datos <- read.csv("Datos/LA MOLINA 2014 POTATO WUE (FB) - fb.csv"
+                  , header = TRUE, sep = ",")
+Datos <- read.delim("Datos/LA MOLINA 2014 POTATO WUE (FB) - fb.tsv", 
+                  header = TRUE, sep = "\t")
+datos <- read.delim("Datos/LA MOLINA 2014 POTATO WUE (FB) - fb.tsv", 
+                    header = TRUE, sep = "\t")
